52b1455e6c94516a7ded27fac89e8e496317e16d
mihaGP800 <mihagoroh@gmail.com>
2022-05-26 22:10:50 +0300

    variables_mixins

--- src/4-Projects/Project/Project.tsx
+++ src/4-Projects/Project/Project.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import s from './Project.module.css'
+import s from './Project.module.scss'
 
 type ProjectType = {
     title: string
     desc: string
+    style: { color: string, backgroundImage: string }
 }
 
-export const Project:React.FC<ProjectType> = ({title,desc}) => {
+export const Project:React.FC<ProjectType> = ({title,desc, style}) => {
     return (
         <div className={s.project}>
-            <div className={s.image}>
-                <button>view</button>
+            <div className={s.image} style={style}>
+                <button className={s.button}>view</button>
             </div>
             <h3 className={s.title}>{title}</h3>
             <span className={s.desc}>{desc}</span>
--- src/4-Projects/Projects.tsx
+++ src/4-Projects/Projects.tsx
@@ -3,27 +3,35 @@ import s from './Projects.module.css'
 import sContainer from '../common/styles/Conteiner.module.css';
 import {Project} from './Project/Project';
 import {Title} from '../common/components/title/Title';
-
+import socNet from '../assets/image/socialNetwork.jpg'
+import todo from '../assets/image/todolist.png'
 
 
 export const Projects = () => {
     const socialNetwork = {
         color: 'blue',
-        backgroundImage: 'url(' + imgUrl + ')',
+        backgroundImage: `url(${socNet})`,
     };
 
-
+    const todolist = {
+        color: 'blue',
+        backgroundImage: `url(${todo})`,
+    };
 
     return (
         <div className={s.projectsBlock}>
             <div className={`${sContainer.container} ${s.projectsContainer}`}>
 
                 <Title title1={'Portfolio'}
-                title2={'My Amazing Works'}/>
+                       title2={'My Amazing Works'}/>
 
                 <div className={s.projects}>
-                    <Project title={'To Do List'} desc={'Simple tool to organise everything'}/>
-                    <Project title={'Social Network'} desc={'an online platform which people use to build social networks'}/>
+                    <Project title={'To Do List'}
+                             style={todolist}
+                             desc={'Simple tool to organise everything'}/>
+                    <Project title={'Social Network'}
+                             style={socialNetwork}
+                             desc={'an online platform which people use to build social networks'}/>
                 </div>
 
             </div>
